4aa752093b7ff1b8510950957d67f1262a4c4a95
SVasiliy <vasiliy.sharapov@gmail.com>
2018-08-24 14:12:05 -0500

    add project datamodel

---
+++ src/main/java/droolscours/Account.java
@@ -0,0 +1,21 @@
+package droolscours;
+
+public class Account {
+
+	private long accountNo;
+	private double balance;
+	
+	public long getAccountno() {
+		return accountNo;
+	}
+	public void setAccountno(long accountno) {
+		this.accountNo = accountno;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+		
+}
