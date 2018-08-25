34177076cea1fe130312c182f9f245c5fa62441c
SVasiliy <vasiliy.sharapov@gmail.com>
2018-08-24 19:56:40 -0500

    implement first rule

---
+++ src/test/droolscours/TestLesson1.java
@@ -0,0 +1,33 @@
+package droolscours;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+
+import util.KnowledgeSessionHelper;
+
+@SuppressWarnings("restriction")
+public class TestLesson1 {
+	
+	StatelessKieSession sessionStateless = null;
+	KieSession sessionStatefull = null;
+	static KieContainer kieContainer;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		kieContainer = KnowledgeSessionHelper.createRuleBase();
+	}
+
+	@Test
+	public void test() {
+		sessionStatefull = KnowledgeSessionHelper.getStatefullKnowledgeSession(kieContainer, "ksession-rules");
+		Account account = new Account();
+		sessionStatefull.insert(account);
+		sessionStatefull.fireAllRules();
+	}
+
+}
